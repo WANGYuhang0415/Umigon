21ae8095c1179282bf41413050330d99b49caaa7
e <yuhang.wang@etu.emse.fr>
2017-12-01 16:07:08 +0100

    about page structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatSidenavModule,MatCardModule, MatGridListModule, MatIconModule, MatTableModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatSidenavModule,MatCardModule, MatGridListModule, MatIconModule, MatTableModule, MatExpansionModule, MatTabsModule } from '@angular/material';
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 
@@ -37,7 +37,9 @@ import { SubNavComponent } from './subNav.component';
     MatGridListModule,
     MatCardModule,
     MatIconModule,
-    MatTableModule
+    MatTableModule,
+    MatExpansionModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
